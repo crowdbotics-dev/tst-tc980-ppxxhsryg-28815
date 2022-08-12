269a26317dba54880cedc9f0e0929c60a5193ea3
Crowdbotics <engineering@crowdbotics.com>
2022-08-12 00:34:18 +0000

    Update models

--- backend/tst_tc980_ppxxhsryg_28815/wsgi.py
+++ backend/tst_tc980_ppxxhsryg_28815/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tst_tc980_ppxxhsryg_28815.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tst_tc980_ppxxhsryg_28815.settings")
 
 application = get_wsgi_application()
